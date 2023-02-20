9f252bfc0290b6429fe3459d160595e976d7cb13
Jakub <jakub017@op.pl>
2023-02-20 17:57:47 +0100

    Add Post to DB

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -1,32 +1,68 @@
-'use client'
+"use client"
 
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
+import toast from "react-hot-toast"
+import axios, { AxiosError } from "axios"
 
-export const AddPost = () => {
-    const [title, setTitle] = useState('')
+export default function CreatePost() {
+    const [title, setTitle] = useState("")
     const [isDisabled, setIsDisabled] = useState(false)
+    const queryClient = useQueryClient()
+    let toastPostID: string
+
+    //Create a post
+    const { mutate } = useMutation(
+        async (title: string) =>
+            await axios.post("/api/posts/addPost", {
+                title,
+            }),
+        {
+            onError: (error) => {
+                if (error instanceof AxiosError) {
+                    toast.error(error?.response?.data.message, { id: toastPostID })
+                }
+                setIsDisabled(false)
+            },
+            onSuccess: (data) => {
+                queryClient.invalidateQueries(["posts"])
+                toast.success("Post has been made 🔥", { id: toastPostID })
+                setTitle("")
+                setIsDisabled(false)
+            },
+        }
+    )
+    const submitPost = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsDisabled(true)
+        toastPostID = toast.loading("Creating your post", { id: toastPostID })
+        mutate(title)
+    }
 
     return (
-        <form className="p-8 my-8 bg-white rounded-md">
+        <form onSubmit={submitPost} className="p-8 my-8 bg-white rounded-md ">
             <div className="flex flex-col my-4">
                 <textarea
                     onChange={(e) => setTitle(e.target.value)}
-                    name='title'
                     value={title}
+                    name="title"
                     placeholder="What's on your mind?"
-                    className="p-4 text-lg bg-gray-200 rounded-md"
-                ></textarea>
+                    className="p-4 my-2 text-lg bg-gray-200 rounded-md"
+                />
             </div>
-
-            <div className="flex items-center justify-between gap-2">
-                <p className={`font-bold text-sm ${title.length > 300 ? 'text-red-700' : 'text-gray-700'}`}>{`${title.length}/300`}</p>
+            <div className="flex items-center justify-between gap-2 ">
+                <p
+                    className={`font-bold text-sm ${title.length > 300 ? "text-red-700" : "text-gray-700"
+                        } `}
+                >{`${title.length}/300`}</p>
                 <button
                     disabled={isDisabled}
-                    className='px-6 py-2 text-sm text-white bg-teal-600 rounded-xl disabled:opacity-25'
-                    type='submit'
-                >Create a post</button>
+                    className="px-6 py-2 text-sm text-white bg-teal-600 rounded-xl disabled:opacity-25"
+                    type="submit"
+                >
+                    Create post
+                </button>
             </div>
         </form>
     )
 }
-
