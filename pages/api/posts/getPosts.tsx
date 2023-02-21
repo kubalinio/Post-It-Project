1af6679bd06ebc9f06e3663a647f36cf7141f500
Jakub <jakub017@op.pl>
2023-02-21 13:53:10 +0100

    Created getPosts & Component Post for receiving posts

---
+++ pages/api/posts/getPosts.tsx
@@ -0,0 +1,24 @@
+import prisma from "../../../prisma/index"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "GET") {
+        //Fetch all posts
+        try {
+            const data = await prisma.post.findMany({
+                include: {
+                    user: true,
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            })
+            res.status(200).json(data)
+        } catch (err) {
+            res.status(403).json({ message: "Error fetching posts" })
+        }
+    }
+}
