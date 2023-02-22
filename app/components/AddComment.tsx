f7bbb8f1f60fd22ac0f807e14fd68f20a9234942
Jakub <jakub017@op.pl>
2023-02-22 19:54:39 +0100

    Created Adding Comments to Detail Post Page

---
+++ app/components/AddComment.tsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { useState } from "react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import axios, { AxiosError } from "axios"
+import toast from "react-hot-toast"
+
+type PostProps = {
+    id?: string
+}
+
+type Comment = {
+    postId?: string
+    title: string
+}
+
+const AddComment = ({ id }: PostProps) => {
+    const [title, setTitle] = useState('')
+    const [isDisabled, setIsDisabled] = useState(false)
+    const queryClient = useQueryClient()
+    let commentToastId: string
+
+    const { mutate } = useMutation(
+        async (data: Comment) => axios.post('/api/posts/addComment', { data }),
+        {
+            onSuccess: data => {
+                setTitle('')
+                setIsDisabled(false)
+                queryClient.invalidateQueries(['detail-post'])
+                toast.success('Added your comment', { id: commentToastId })
+            },
+            onError: error => {
+                setIsDisabled(false)
+                if (error instanceof AxiosError) {
+                    toast.error(error?.response?.data.message, { id: commentToastId })
+                }
+            }
+        }
+    )
+
+    const submitComment = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setIsDisabled(true)
+        commentToastId = toast.loading('Adding your comment', { id: commentToastId })
+        mutate({ title, postId: id })
+    }
+
+
+    return (
+        <form onSubmit={submitComment}>
+            <h3>Add a comment</h3>
+            <div className="flex flex-col my-2">
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    type="text"
+                    name="title"
+                    className="p-4 my-2 text-lg rounded-md"
+                />
+            </div>
+            <div className="flex items-center gap-2">
+                <button
+                    disabled={isDisabled}
+                    className='px-6 py-2 text-sm text-white bg-teal-600 rounded-xl disabled:opacity-50'
+                    type="submit"
+                >
+                    Add Comment
+                </button>
+                <p className={`font-bold ${title.length > 300 ? 'text-red-700' : 'text-gray-700'}`}>
+                    {`${title.length}/300`}
+                </p>
+            </div>
+        </form>
+    )
+}
+
+export default AddComment
--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,6 +3,8 @@
 import Post from '../../components/Post'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import AddComment from '@/app/components/AddComment'
+import Image from 'next/image'
 // import { PostType } from '@/app/types/Post'
 
 type URL = {
@@ -23,11 +25,28 @@ const PostDetail = (url: URL) => {
     })
     if (isLoading) return <div>Loading...</div>
 
-    console.log(data)
 
     return (
         <div>
             <Post id={data.id} name={data.user.name} avatar={data.user.image} postTitle={data.title} comments={data.Comment} />
+            <AddComment id={data?.id} />
+            {data?.Comment?.map((comment) => (
+                <div key={comment.id} className='p-8 my-6 bg-white rounded-md' >
+                    <div className='flex items-center gap-2'>
+                        <Image
+                            width={24}
+                            height={24}
+                            src={comment.user?.image}
+                            alt='avatar'
+                        />
+                        <h3 className='font-bold'>{comment?.user?.name}</h3>
+                        <h2 className='text-sm'>{comment.createdAt}</h2>
+                    </div>
+                    <div className='py-4'>
+                        {comment.message}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
